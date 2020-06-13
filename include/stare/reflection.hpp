d513a24affd3ecb9b07fc3a4df71545848a6cf5e
Frederik Engels <frederik.engels92@gmail.com>
2020-06-13 16:43:55 +0200

    concept to check whether a type is reflected

--- include/stare/reflection.hpp
+++ include/stare/reflection.hpp
@@ -16,21 +16,35 @@ namespace detail
 template<class T>
 struct reflection_interface
 {
-    /// the type's name as interpreted by the compiler
+    /// the type's name as interpreted by the compiler.
+    /// This name is implementation defined to the specific compiler and cannot
+    /// reliably be used for cross compiler operations, you should use the user
+    /// defined name() instead.
     constexpr auto internal_name() const noexcept
     {
         return stare::nameof_type<T>();
     }
 
+    /// if no bases are defined this will return an empty type list
     constexpr auto bases() const noexcept
     {
         return stare::type_list<>{};
     }
 
+    /// if no fields are defined this will return an empty tuple
     constexpr auto fields() const noexcept
     {
         return std::make_tuple();
     }
 };
+
+template<typename T>
+concept reflection_type_complete = requires
+{
+    ::stare::reflection<T>{};
+};
 } // namespace detail
+
+template<typename T>
+concept reflected = stare::detail::reflection_type_complete<T>;
 } // namespace stare
