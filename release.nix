3f52d7e204d577f0c08e4051c2d8916f8846883e
Frederik Engels <frederik.engels92@gmail.com>
2020-06-22 15:19:26 +0200

    add a default nix release without override for stdenv

--- release.nix
+++ release.nix
@@ -1,15 +1,20 @@
 { nixpkgs ? <nixpkgs> }:
 let
-    pkgs = import nixpkgs {config = {}; };
-    pkgs-aarch64 = import nixpkgs { system="aarch64-linux"; config = {}; };
+    pkgs = import nixpkgs { };
+
+    pkgs-android = import nixpkgs {
+        crossSystem = (import <nixpkgs/lib>).systems.examples.aarch64-android-prebuilt;
+        config = {
+            allowUnsupportedSystem = true;
+        };
+    };
 
     jobs = rec {
+        nix = pkgs.callPackage ./derivation.nix { };
         nix-gcc9 = pkgs.callPackage ./derivation.nix { stdenv = pkgs.gcc9Stdenv; };
         nix-gcc10 = pkgs.callPackage ./derivation.nix { stdenv = pkgs.gcc10Stdenv; };
 
         nix-clang_10 = pkgs.callPackage ./derivation.nix { stdenv = with pkgs; overrideCC stdenv clang_10; };
-
-        nix-gcc10-aarch64 = pkgs-aarch64.callPackage ./derivation.nix { stdenv = pkgs-aarch64.gcc10Stdenv; };
     };
 in
     jobs
